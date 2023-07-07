f416dc2670216306f0854ebc581cfb21b809f022
mustafa <musmoh73@gmail.com>
2023-07-08 00:28:41 +0300

    style popup

--- src/ProductDetalis.jsx
+++ src/ProductDetalis.jsx
@@ -1,8 +1,5 @@
+import { Input,Button } from "@material-tailwind/react";
 import React from "react";
-import { Button, FloatingLabel } from "react-bootstrap";
-import Col from 'react-bootstrap/Col';
-import Form from 'react-bootstrap/Form';
-import Row from 'react-bootstrap/Row';
 
 const ProductDetails = ({
   handlePopupToggle,
@@ -13,109 +10,36 @@ const ProductDetails = ({
 }) => {
   return (
     <>
-      {/* <div className="modal">
+      <div className="modal">
         <div onClick={handlePopupToggle} className="overlay"></div>
         <div className="modal-content">
-          <div className="flex"> */}
-          {/* <Form>
-      <Row>
-        <Col>
-          <Form.Control placeholder="First name" />
-        </Col>
-        <Col>
-          <Form.Control placeholder="Last name" />
-        </Col>
-      </Row>
-    </Form> */}
+          <div className="flex">
+          <div className="flex flex-col w-96 mt-4 mb-4 items-start gap-8">
+      <Input size="lg" label="name" />
+      <Input size="lg" label="location" />
+      <Input size="lg" label="phone number" />
+      <div className="flex items-center ">
+        <div className=" ffff">
+           <a href="#buttons-with-link">
+        <Button onClick={handleByNowClick}>By Now</Button>
+      </a>
+        </div>
+     
+      
+   
+      
+    </div>
+    </div>
             {/* <input type="text" name="input1" onChange={handleInput1Change} />
             <input type="text" name="input2" onChange={handleInput2Change} />
-            <input type="text" name="input3"  onChange={handleInput3Change} />
-            <button onClick={handleByNowClick}>By Now</button>
-          </div> */}
+            <input type="text" name="input3"  onChange={handleInput3Change} /> */}
+            {/* <button onClick={handleByNowClick}>By Now</button> */}
+          </div>
           {/* <button className="close-modal" onClick={handlePopupToggle}>
             CLOSE
           </button> */}
-        {/* </div>
-        
-      </div> */}
-      {/* <section className="r">
-      <div onClick={handlePopupToggle} className="overlay"></div>
-         <Form className="modal-content">
-      <Row className="mb-3">
-        <Form.Group as={Col} controlId="formGridEmail">
-          <Form.Label>Email</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
-        </Form.Group>
-
-        <Form.Group as={Col} controlId="formGridPassword">
-          <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
-        </Form.Group>
-      </Row>
-
-      <Form.Group className="mb-3" controlId="formGridAddress1">
-        <Form.Label>Address</Form.Label>
-        <Form.Control placeholder="1234 Main St" />
-      </Form.Group>
-
-      <Form.Group className="mb-3" controlId="formGridAddress2">
-        <Form.Label>Address 2</Form.Label>
-        <Form.Control placeholder="Apartment, studio, or floor" />
-      </Form.Group>
-
-      <Row className="mb-3">
-        <Form.Group as={Col} controlId="formGridCity">
-          <Form.Label>City</Form.Label>
-          <Form.Control />
-        </Form.Group>
-
-        <Form.Group as={Col} controlId="formGridState">
-          <Form.Label>State</Form.Label>
-          <Form.Select defaultValue="Choose...">
-            <option>Choose...</option>
-            <option>...</option>
-          </Form.Select>
-        </Form.Group>
-
-        <Form.Group as={Col} controlId="formGridZip">
-          <Form.Label>Zip</Form.Label>
-          <Form.Control />
-        </Form.Group>
-      </Row>
-
-      <Form.Group className="mb-3" id="formGridCheckbox">
-        <Form.Check type="checkbox" label="Check me out" />
-      </Form.Group>
-
-      <Button variant="primary" type="submit">
-        Submit
-      </Button>
-      </Form>
-      </section> */}
-  <div
-        className="popuplog "
-        style={{ minHeight: "100vh" }}
-      >
-        <div className="w-100" style={{ maxWidth: "400px" }}>
-          <h2 className="text-center mb-4">Log In</h2>
-          <Form >
-            <Form.Group>
-              <Form.Label htmlFor="email">Email</Form.Label>
-              <Form.Control id="email" type="email"  required />
-            </Form.Group>
-            <Form.Group>
-              <Form.Label htmlFor="password">Password</Form.Label>
-              <Form.Control
-                id="password"
-                type="password"
-              />
-            </Form.Group>
-            <Button  className="w-100 mt-3" type="submit">
-              Log In
-            </Button>
-          </Form>
-       
         </div>
+        
       </div>
     </>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Input } from "@material-tailwind/react";
 
 import Card from '../components/Card';
 import { useState } from 'react';
@@ -123,11 +122,7 @@ export default function Home() {
     <>
     <div className="app">
     <Homepg/>
-    <div className="flex flex-col w-72 gap-6">
-      <Input variant="static" label="Static" placeholder="Static" />
-      <Input variant="standard" label="Standard" />
-      <Input variant="outlined" label="Outlined" />
-    </div>
+    
       <section className='pro'>
     <div className='fex titles'> <h2>shapping</h2></div> 
       <div className='r'>
