35056a51d717c4d683f6f92d4b1b3fe56c62fd1c
mustafa <musmoh73@gmail.com>
2023-07-08 21:21:02 +0300

    mangr dashbord style

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,5 +1,6 @@
 import React, { useState,useEffect } from "react";
 import { db, storage } from "../components/firebase";
+import Items from './authlogin/Items';
 
 const Dashboard = () => {
   const [imageFile1, setImageFile1] = useState(null);
@@ -108,11 +109,47 @@ const Dashboard = () => {
     <section className='pro'>
     <h1 className="fex titles">Dashboard</h1>
       <form onSubmit={handleSubmit}>
-        <label>Image 1:</label>
-        <input type="file" onChange={(e) => handleImageChange(e, setImageFile1)} />
+        
+      <div>
+            <label for="first_name" class="block mb-2 text-sm font-medium text-gray-200 dark:text-white">title</label>
+            <input type="text" id="first_name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-64 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="John" />
+        </div>
+      
+        <label class="block mb-2 text-sm font-medium text-gray-200 dark:text-white" for="file_input">glass 2</label>
+        <input  onChange={(e) => handleImageChange(e, setImageFile2)}  class="block w-64 text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" id="file_input" type="file"/>
 
-        <label>Image 2:</label>
-        <input type="file" onChange={(e) => handleImageChange(e, setImageFile2)} />
+          
+    <div class="grid gap-6 mb-6 md:grid-cols-2">
+          <div className="flexinput">
+
+         
+        <label class="block mb-2 text-sm font-medium text-gray-200 dark:text-white" for="file_input">glass 1</label>
+        <input  onChange={(e) => handleImageChange(e, setImageFile1)} class="block w-64   mb-5 text-lg text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" id="file_input" type="file"/>
+     </div>
+        <div>
+            <label for="last_name" class="block mb-2 text-sm font-medium text-gray-200 dark:text-white">color 1</label>
+            <input type="text" id="last_name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-64 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Doe" />
+        </div>
+       
+      
+        
+       
+    </div>
+   
+<label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white" for="small_size">Small file input</label>
+<input class="block w-full mb-5 text-xs text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" id="small_size" type="file"/>
+
+<label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white" for="default_size">Default size</label>
+<input class="block w-full mb-5 text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" id="default_size" type="file"/>
+
+<label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white" for="large_size">Large file input</label>
+<input class="block w-full text-lg text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" id="large_size" type="file"/>
+
+  
+   
+   
+    <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-64 sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
+
 
         <label>Title:</label>
         <input
