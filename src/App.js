3c7463457cebc51104e38521e46ac432b621ace5
mustafa <musmoh73@gmail.com>
2023-07-09 00:30:51 +0300

    add outlit

--- src/App.js
+++ src/App.js
@@ -1,36 +1,47 @@
-import { Route, Routes } from 'react-router-dom';
-import Layout from './layout';
-import Home from './pages/Home';
-import Loading from './components/Loading';
-import Baskett from './pages/Baskett';
-import Requests from './components/Requests';
-import Dashboard from './components/Dashboard';
-import LoginPage from './components/LoginPage';
-import Login from './components/authlogin/Login';
-import Singup from './components/authlogin/Singup';
-import ForgotPassword from './components/authlogin/ForgotPassword'
-import AuthProvider from './context/AuthContext';
-import RequireAuth from './context/RequirAuth';
-import Dashboardd from './components/authlogin/Dashboardd';
+import { Route, Routes } from "react-router-dom";
+import Layout from "./layout";
+import Home from "./pages/Home";
+import Loading from "./components/Loading";
+import Baskett from "./pages/Baskett";
+import Requests from "./components/Requests";
+import Dashboard from "./components/Dashboard";
+import LoginPage from "./components/LoginPage";
+import Login from "./components/authlogin/Login";
+import Singup from "./components/authlogin/Singup";
+import ForgotPassword from "./components/authlogin/ForgotPassword";
+import AuthProvider from "./context/AuthContext";
+import RequireAuth from "./context/RequirAuth";
+import Dashboardd from "./components/authlogin/Dashboardd";
 function App() {
   return (
-    <>   
-         <AuthProvider>
-      <Layout>
-        <Routes>
-          <Route path='/' element={<RequireAuth><Home/></RequireAuth>} />
-          <Route path='/Baskett' element={<RequireAuth><Baskett /></RequireAuth>} />
-          <Route path='/Loading' element={<Loading />} />
-          <Route  path="/requests" element={<Requests/>} />
-          <Route  path="/Dashboard" element={ <Dashboard/>} />
-          <Route  path="/LoginPage" element={<LoginPage />} />
-          <Route  path="/login" element={<Login/>} />
-          <Route path='/forgot-password' element={<ForgotPassword />} />
-          <Route path='/Singup' element={<Singup />} />
-          <Route path='/Dashboardd' element={<RequireAuth><Dashboardd /></RequireAuth>} />
-        </Routes>
-      </Layout>        </AuthProvider>
+    <>
+      <AuthProvider>
+          <Routes>         
+                 <Route path="/login" element={<Login />} />
 
+            <Route element={<Layout />}><Route
+                path="/"
+element={<RequireAuth><Home /> </RequireAuth> }
+              />
+
+              <Route
+                path="/Baskett"
+                element={
+                  <RequireAuth><Baskett /></RequireAuth> }
+              />
+              <Route path="/Loading" element={<Loading />} />
+              <Route path="/requests" element={<Requests />} />
+              <Route path="/Dashboard" element={<Dashboard />} />
+              <Route path="/LoginPage" element={<LoginPage />} />
+              <Route path="/forgot-password" element={<ForgotPassword />} />
+              <Route path="/Singup" element={<Singup />} />
+              <Route
+                path="/Dashboardd"
+                element={
+                  <RequireAuth><Dashboardd /></RequireAuth>} />
+            </Route>
+          </Routes>
+      </AuthProvider>
     </>
   );
 }
--- src/layout/index.jsx
+++ src/layout/index.jsx
@@ -1,11 +1,14 @@
 import Footer from './Footer';
 import Navbar from './Navbar';
+import { Outlet } from 'react-router-dom';
 
 export default function Layout({ children }) {
   return (
     <>
       <Navbar />
       {children}
+      <Outlet />
+
       <Footer />
     </>
   );
