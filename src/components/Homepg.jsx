c32727d145571aa892bd685d476d88291abf6f89
mustafa <musmoh73@gmail.com>
2023-07-09 21:15:40 +0300

    style media feet

--- src/components/Homepg.jsx
+++ src/components/Homepg.jsx
@@ -216,19 +216,19 @@ const Homepg = () => {
                   <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
                     <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                       <tr>
-                        <th scope="col" class="px-6 py-3">
+                        <th scope="col" class=" prth px-6 py-3">
                           Product name
                         </th>
-                        <th scope="col" class="px-6 py-3">
+                        <th scope="col" class=" prth px-6 py-3">
                           SPH
                         </th>
-                        <th scope="col" class="px-6 py-3">
+                        <th scope="col" class=" prth px-6 py-3">
                           CYL
                         </th>
-                        <th scope="col" class="px-6 py-3">
+                        <th scope="col" class=" prth px-6 py-3">
                           AIX
                         </th>
-                        <th scope="col" class="px-6 py-3">
+                        <th scope="col" class=" prth px-6 py-3">
                           lens type
                         </th>
                       </tr>
@@ -237,14 +237,14 @@ const Homepg = () => {
                       <tr class="bg-white border-b dark:bg-gray-900 dark:border-gray-700">
                         <th
                           scope="row"
-                          class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                          class=" prth px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
                         >
                           Lift Eye
                         </th>
-                        <td class="px-6 py-4">ffffff</td>
-                        <td class="px-6 py-4">ffffff</td>
-                        <td class="px-6 py-4">ffffff</td>
-                        <td class="px-6 py-4">ffffff</td>
+                        <td class="px-6 py-4 prth">ffffff</td>
+                        <td class="px-6 py-4 prth">ffffff</td>
+                        <td class="px-6 py-4 prth">ffffff</td>
+                        <td class="px-6 py-4 prth">ffffff</td>
                       </tr>
                     
                     </tbody>
