bac6a06d6cc0b7423daf355b7d30a3f2799971db
mustafa <musmoh73@gmail.com>
2023-07-09 23:25:16 +0300

    DEDDW

--- src/components/Homepg.jsx
+++ src/components/Homepg.jsx
@@ -246,7 +246,18 @@ const Homepg = () => {
                         <td class="px-6 py-4 prth">ffffff</td>
                         <td class="px-6 py-4 prth">ffffff</td>
                       </tr>
-                    
+                      <tr class="bg-white border-b dark:bg-gray-900 dark:border-gray-700">
+                        <th
+                          scope="row"
+                          class=" prth px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                        >
+                          Lift Eye
+                        </th>
+                        <td class="px-6 py-4 prth">ffffff</td>
+                        <td class="px-6 py-4 prth">ffffff</td>
+                        <td class="px-6 py-4 prth">ffffff</td>
+                        <td class="px-6 py-4 prth">ffffff</td>
+                      </tr>
                     </tbody>
                   </table>
                 </div>
