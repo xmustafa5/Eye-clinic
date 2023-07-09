eceee73d991ae360284aa86596dac7fd5a80fcde
mustafa <musmoh73@gmail.com>
2023-07-09 16:52:44 +0300

    fix inputs

--- src/components/Homepg.jsx
+++ src/components/Homepg.jsx
@@ -213,7 +213,7 @@ const Homepg = () => {
             </div>
         </div> 
          </div>
-         <div>
+         <div className="flexo ">
          <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
                   <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
                     <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
