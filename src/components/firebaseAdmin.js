0eb8e283d12e1e8752d892b0cfa27c9fe1f5852a
mustafa <musmoh73@gmail.com>
2023-07-10 16:29:40 +0300

    Add apiKey

--- src/components/firebaseAdmin.js
+++ src/components/firebaseAdmin.js
@@ -7,21 +7,15 @@ import 'firebase/compat/auth';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  // apiKey: "AIzaSyCKPwAxf7lc2M0gt8aQAdyCJ9N4vaF_Os0",
-  // authDomain: "eye-clinic-77ea0.firebaseapp.com",
-  // projectId: "eye-clinic-77ea0",
-  // storageBucket: "eye-clinic-77ea0.appspot.com",
-  // messagingSenderId: "455750057162",
-  // appId: "1:455750057162:web:b012561b2b1eab7ec8c833",
-  // measurementId: "G-YFYDSVJ8WL"
-  apiKey: "AIzaSyB1i2k80PmyVZl6kdRaCsq5gM2BlC8KzYc",
-  authDomain: "glassesd-cd48f.firebaseapp.com",
-  projectId: "glassesd-cd48f",
-  storageBucket: "glassesd-cd48f.appspot.com",
-  messagingSenderId: "862054620672",
-  appId: "1:862054620672:web:a61cf1f3a3d30e741d02e5",
-  measurementId: "G-DFJWK4PHSG"
-};
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+  };
+  
 
 
 
@@ -29,11 +23,11 @@ const firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 
 // Initialize Firestore
-export const db = firebase.firestore();
+export const dbA = firebase.firestore();
 
 // Initialize Storage
-export const storage = firebase.storage();
-export const auth = firebase.auth();
+export const storageA = firebase.storage();
+export const authA = firebase.auth();
 
 // You can also export other Firebase services if needed
 
