a9a815f7027a0fed752133c281b8e4bbb584ccc7
mustafa <musmoh73@gmail.com>
2023-07-11 19:09:58 +0300

    style loading page

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
-import ReactLoading from 'react-loading';
 import './loading.css';
 const Loading = (props) => (
   <div className='loading'>
-        <ReactLoading type={props.spin 
-} color='#30a3e6'/>
-        {/* <ReactLoading type={spokes} color='black' />
-        <ReactLoading type={spokes} color='black' /> */}
-  </div>
-);
+<div className="lds-ring ">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+    </div>);
 
 export default Loading;
