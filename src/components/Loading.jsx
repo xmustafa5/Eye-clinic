b3cfa80b42c76c2b78ab8cfb155839bbdb818178
mustafa <musmoh73@gmail.com>
2023-07-11 19:16:22 +0300

    style loading 2

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,13 +1,18 @@
-import React from 'react';
-import './loading.css';
+import React from "react";
+import "./loading.css";
 const Loading = (props) => (
-  <div className='loading'>
-<div className="lds-ring ">
+  <div className="loading">
+    <div class="lds-roller">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
       <div></div>
       <div></div>
       <div></div>
       <div></div>
     </div>
-    </div>);
+  </div>
+);
 
 export default Loading;
