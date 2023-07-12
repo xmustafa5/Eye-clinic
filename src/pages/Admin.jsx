5a1c163d5d7b92b07246587b6d386e2d0dbb9733
mustafa <musmoh73@gmail.com>
2023-07-12 13:08:28 +0300

    fix admin shape

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -1,29 +1,26 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import "./Admin.css"
-import Items from './../components/authlogin/Items';
+import "./Admin.css";
+import Items from "./../components/authlogin/Items";
 const Admin = () => {
-    const handleLogout = () => {
-        // Clear the authentication status in localStorage
-        localStorage.removeItem('isLoggedIn');
-        // Redirect to the login page
-        window.location.href = '/logadmin';
-      };
+  const handleLogout = () => {
+    // Clear the authentication status in localStorage
+    localStorage.removeItem("isLoggedIn");
+    // Redirect to the login page
+    window.location.href = "/logadmin";
+  };
   return (
-    <section className="pro colorize" >
-      <div className="r ccc">  
-      
-           <div class="containero">
-        <div class="shapeW">
-
-          <h1 className="admitt">Admin</h1>
+    <section className="pro colorize">
+      <div className="r ccc">
+        <div className="dddd">
+          <div class="containero">
+            <div class="shapeW">
+              <h1 className="admitt">Admin</h1>
+            </div>
+          </div>
         </div>
-      </div>
         <div className="dddde">
-       
-      
-      <div className={"homebtngroup mb-4 "}>
-     
+          <div className={"homebtngroup mb-4 "}>
             <Link to="/requests">
               <button
                 className={"btnbtnprimary"}
@@ -44,13 +41,14 @@ const Admin = () => {
                 <span className={"square"}></span>
               </button>
             </Link>
-            
           </div>
           <div className="flex justify-center align-Items-center">
-      <button className="colse_admin" onClick={handleLogout}>Close Admin</button>
-    </div>
-      </div>
+            <button className="colse_admin" onClick={handleLogout}>
+              Close Admin
+            </button>
+          </div>
         </div>
+      </div>
     </section>
   );
 };
