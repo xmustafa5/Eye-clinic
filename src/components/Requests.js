5090c94d2396a14a4c639fb4119ece0eba7189b2
mustafa <musmoh73@gmail.com>
2023-07-14 16:29:02 +0300

    fix bton finel

--- src/components/Requests.js
+++ src/components/Requests.js
@@ -86,125 +86,117 @@ const Requests = () => {
         <ul className="content d">
           {requestItems.map((request) => (
             <li className="borditem " key={request.id}>
-              <div className="pb-4">
-
-            
-              <div className="fex inputs">
-                <div className="flex w-full justify-center text-lg mb-2">
-                     <p> {request.input1}</p>
-                <p> - {request.input2}</p>
-                <p>- {request.input3}</p>
-                </div>
-             
-                <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
-          <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-            <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-              <tr>
-                <th scope="col" class="vvv  prth  px-6 py-3"> 
-                  Product name
-                </th>
-                <th scope="col" class="vvv  prth px-6 py-3">
-                  SPH
-                </th>
-                <th scope="col" class=" vvv prth px-6 py-3">
-                  CYL
-                </th>
-                <th scope="col" class="vvv  prth px-6 py-3">
-                  AIX
-                </th>
-                <th scope="col" class="vvv  prth px-6 py-3">
-                  lens type
-                </th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr class="bg-white border-b dark:bg-gray-900 dark:border-gray-700">
-                <th
-                  scope="row"
-                  class="vvv px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-                >
-                  Lift Eye
-                </th>
-                <td class="vvv px-6 py-4">{request.input8}</td>
-                <td class="vvv px-6 py-4">{request.input8}</td>
-                <td class="vvv px-6 py-4">{request.input8}</td>
-                <td class="vvv px-6 py-4">{request.input8}</td>
-              </tr>
-              <tr class="bg-white border-b dark:bg-gray-900 dark:border-gray-700">
-                <th
-                  scope="row"
-                  class="vvv px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-                >
-                  Right Eye
-                </th>
-                <td class="vvv px-6 py-4">{request.input8}</td>
-                <td class="vvv px-6 py-4">{request.input8}</td>
-                <td class="vvv px-6 py-4">{request.input8 }</td>
-                <td class="vvv px-6 py-4"></td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
-                <h1 className="text-xl mt-2">Glasses:</h1>
-              </div>
-              <div>
-    
-       
-     
-    </div>
-
-              {request.items && request.items.length > 0 ? (
-                <ul className="contento pb-4">
-                  {request.items.map((item) => (
-                    <li key={item.id}>
-                      <div className="projcardo">
-                      <div className="sssso">
-                  <div className="projimg">
-                    <img src={item.imageUrl} alt="Selected Option" />
+              <div className="pb-2">
+                <div className="fex inputs">
+                  <div className="flex w-full justify-center text-lg mb-2">
+                    <p> {request.input1}</p>
+                    <p> - {request.input2}</p>
+                    <p>- {request.input3}</p>
                   </div>
-                  {/* <img src={imageSource} alt="Selected Option"  /> */}
+                  <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+                    <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+                      <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                        <tr>
+                          <th scope="col" class="vvv  prth  px-6 py-3">
+                            Product name
+                          </th>
+                          <th scope="col" class="vvv  prth px-6 py-3">
+                            SPH
+                          </th>
+                          <th scope="col" class=" vvv prth px-6 py-3">
+                            CYL
+                          </th>
+                          <th scope="col" class="vvv  prth px-6 py-3">
+                            AIX
+                          </th>
+                          <th scope="col" class="vvv  prth px-6 py-3">
+                            lens type
+                          </th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        <tr class="bg-white border-b dark:bg-gray-900 dark:border-gray-700">
+                          <th
+                            scope="row"
+                            class="vvv px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                          >
+                            Lift Eye
+                          </th>
+                          <td class="vvv px-6 py-4">{request.input8}</td>
+                          <td class="vvv px-6 py-4">{request.input8}</td>
+                          <td class="vvv px-6 py-4">{request.input8}</td>
+                          <td class="vvv px-6 py-4">{request.input8}</td>
+                        </tr>
+                        <tr class="bg-white border-b dark:bg-gray-900 dark:border-gray-700">
+                          <th
+                            scope="row"
+                            class="vvv px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                          >
+                            Right Eye
+                          </th>
+                          <td class="vvv px-6 py-4">{request.input8}</td>
+                          <td class="vvv px-6 py-4">{request.input8}</td>
+                          <td class="vvv px-6 py-4">{request.input8}</td>
+                          <td class="vvv px-6 py-4"></td>
+                        </tr>
+                      </tbody>
+                    </table>
+                  </div>{" "}
+                  <h1 className="text-xl mt-2">Glasses:</h1>
                 </div>
-                        <div className="projinfo">
-                          <strong className="projtitle">
-                            <span className="titlecard">{item.title} </span>
-                            <div className="centerbrn"> 
-                              {item.color && (
-                                <button className={`radio-buttono`}>
-                                  {item.color}
-                                </button>
-                              )}
+                <div></div>
+
+                {request.items && request.items.length > 0 ? (
+                  <ul className="contento ">
+                    {request.items.map((item) => (
+                      <li key={item.id}>
+                        <div className="projcardo">
+                          <div className="sssso">
+                            <div className="projimg">
+                              <img src={item.imageUrl} alt="Selected Option" />
                             </div>
-                          </strong>
-                          <div className="prices">
-                            <p className="iopp">{item.price}$</p>
+                            {/* <img src={imageSource} alt="Selected Option"  /> */}
                           </div>
-                          <div className="fexbtn">
-                          <div className={"homebtngroup1"}>
-                    <button
-                      className={"btnbtnprimary"}
-                      onClick={() =>
-                        removeItemFromRequest(
-                          request.id,
-                          item.id,
-                          request.items.length
-                        )
-                      }                    >
-                      <p className={"btntext4"}>remove</p>
-                      <span className={"square"}></span>
-                    </button>
-                  </div>
-                         
+                          <div className="projinfo">
+                            <strong className="projtitle">
+                              <span className="titlecard">{item.title} </span>
+                              <div className="centerbrn">
+                                {item.color && (
+                                  <button className={`radio-buttono`}>
+                                    {item.color}
+                                  </button>
+                                )}
+                              </div>
+                            </strong>
+                            <div className="prices">
+                              <p className="iopp">{item.price}$</p>
+                            </div>
+                            <div className="fexbtn">
+                              <div className={"homebtngroup1"}>
+                                <button
+                                  className={"btnbtnprimary"}
+                                  onClick={() =>
+                                    removeItemFromRequest(
+                                      request.id,
+                                      item.id,
+                                      request.items.length
+                                    )
+                                  }
+                                >
+                                  <p className={"btntext4"}>remove</p>
+                                  <span className={"square"}></span>
+                                </button>
+                              </div>
+                            </div>
+                          </div>
                         </div>
-                        </div>
-                     
-                      </div>
-                    </li>
-                  ))}
-                </ul> 
-              ) : (
-                <p>No items in the request.</p>
-              )}
- </div>
+                      </li>
+                    ))}
+                  </ul>
+                ) : (
+                  <p>No items in the request.</p>
+                )}
+              </div>
               <div className="fexbtn">
                 <button
                   className="button-29"
