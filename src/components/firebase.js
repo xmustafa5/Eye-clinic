62f016a7c26a3660e900bf6fc61d754ba7d0ce7b
mustafa <musmoh73@gmail.com>
2023-07-19 13:48:02 +0300

    style shop

--- src/components/firebase.js
+++ src/components/firebase.js
@@ -4,16 +4,8 @@
   import 'firebase/compat/auth';
 
 
-  // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
   const firebaseConfig = {
-    // apiKey: "AIzaSyCKPwAxf7lc2M0gt8aQAdyCJ9N4vaF_Os0",
-    // authDomain: "eye-clinic-77ea0.firebaseapp.com",
-    // projectId: "eye-clinic-77ea0",
-    // storageBucket: "eye-clinic-77ea0.appspot.com",
-    // messagingSenderId: "455750057162",
-    // appId: "1:455750057162:web:b012561b2b1eab7ec8c833",
-    // measurementId: "G-YFYDSVJ8WL"
+
     apiKey: "AIzaSyB1i2k80PmyVZl6kdRaCsq5gM2BlC8KzYc",
     authDomain: "glassesd-cd48f.firebaseapp.com",
     projectId: "glassesd-cd48f",
